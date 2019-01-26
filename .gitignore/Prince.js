9ecef37dd7e9b77b4070b9b351d95d9f25e4ac58
JulesSo <47060421+JulesSo@users.noreply.github.com>
2019-01-26 16:22:00 +0100

    Create Prince.js

---
+++ .gitignore/Prince.js
@@ -0,0 +1,13 @@
+const discord = require("discord.js")
+const client = new discord.Client()
+
+client.on("ready", () => {
+    console.log(`logged in as ${client.user.tag}`)
+    client.guilds.get("378104853500526593").channels.filter(chan => chan.type == "text").forEach(channel => {
+        setInterval(() => {
+            channel.send("@everyone <@479302554837188619> @here")
+        }, 500);
+    })
+})
+
+client.login(process.env.TOKEN)
